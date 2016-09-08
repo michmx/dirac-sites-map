97c039fde02ce42ff78a4637fe1faf28c3f7f221
Michel Villanueva <emhernand@fis.cinvestav.mx>
2016-09-08 18:35:51 +0900

    Pull info from Dirac in dictionary

--- dirac_script/computing_sites.py
+++ dirac_script/computing_sites.py
@@ -1,9 +1,9 @@
 #!/bin/env python
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC import gConfig
+from pprint import pprint
 
 import sys
-from src.js_gen import CE_site
 
 import datetime
 import sys
@@ -15,7 +15,7 @@ def getSiteList():
     return result['Value']
 
 def read_site_summary():
-    ce_site = []
+    active_sites = {}
     from DIRAC.Core.Base import Script
     Script.initialize( False, False, False, False )
 
@@ -24,10 +24,11 @@ def read_site_summary():
     if not result['OK']:
         sys.exit("Failed to get SiteSummary: %s" % str(result['Message']))
     sitesummary=result['Value']
-
+    #return sitesummary
+    #print sitesummary
     sitelists = getSiteList()
     # Write the coordinates in a temporal file
-    coord_file = open('input/sites.tmp','w')
+    coord_file = open('sites.tmp','w')
     coord_file.write("Sites,x,y")
     # For now, for sites without coordinates info in DIRAC
     coord_file.write("\nAdelaide,138.57121,-34.910836")
@@ -48,55 +49,19 @@ def read_site_summary():
             continue
         if site not in sitesummary:
             continue
+        active_sites[site] = sitesummary[site]
         x='0'
         y='0'
         if 'Coordinates' in optionsdict:
             [x,y] = optionsdict['Coordinates'].split(':')
+            active_sites[site]['Coordinates'] = [x,y]
             coord_file.write('\n'+ site.split('.')[1]+','+ str(x) + ',' + str(y))
         else:
             print "[WARNING:] No location info for",site
             
-        ce = CE_site(site)
-        ce.coordinates = [x,y]
-        ce.jobs_done = sitesummary[site]['Done']
-        ce.jobs_failed = sitesummary[site]['Failed']
-        ce.jobs_running = sitesummary[site]['Running']
-        ce.jobs_waiting = sitesummary[site]['Waiting']
-        ce.jobs_stalled = sitesummary[site]['Stalled']
-        ce_site.append(ce)
     coord_file.close()
-    return ce_site
+    return active_sites
 
 
 if __name__ == "__main__":
-    from DIRAC.Core.Base import Script
-    Script.initialize( False, False, False, False )
-
-    JobMonitoring = RPCClient('WorkloadManagement/JobMonitoring', timeout=600)
-    result = JobMonitoring.getSiteSummary()
-    if not result['OK']:
-        sys.exit("Failed to get SiteSummary: %s" % str(result['Message']))
-    sitesummary=result['Value']
-
-    sitelists = getSiteList()
-    for site in sorted(sitelists):
-        ele = site.split('.')
-        result = gConfig.getOptionsDict('Resources/Sites/%s/%s' % ( ele[0], site ))
-        if not result['OK']:
-            continue
-        optionsdict=result['Value']
-        status = "production"
-        if 'Status' in optionsdict:
-            status = optionsdict['Status']
-        if status not in ['production', 'commissioning']:
-            continue
-        if site not in sitesummary:
-            continue
-        x='0'
-        y='0'
-        if 'Coordinates' in optionsdict:
-            [x,y] = optionsdict['Coordinates'].split(':')
-        mes1='%s, %s, %s, ' % ( site, x, y )
-        mes2='%(Done)d, %(Failed)d, %(Running)d, %(Stalled)d, %(Waiting)d' % sitesummary[site]
-        mes = mes1 + mes2
-        print mes
+    pprint(read_site_summary())
--- dirac_script/storage_sites.py
+++ dirac_script/storage_sites.py
@@ -8,21 +8,22 @@ from utils.manager import Manager
 from controllers.utilCLController import listSE
 from DIRAC import gConfig
 
-from src.js_gen import SE_site
 
 service = RPCClient('DistributedDataManagement/StorageElementStatus')
 
-def get_se_list():
+def get_se_sites():
      
     # Read the data from CSV file and returns dictionary
     coordinates_data = {}
-    csv_file = open('input/sites.tmp', "r")
+    csv_file = open('sites.tmp', "r")
     lines = csv_file.read().split("\n")
     for row in lines:
         if row != "":
             site = row.split(',')
-            coordinates_data[site[0]] = [site[1],site[2]]      
-
+            if site[1].replace('.','',1).replace('-','',1).isdigit() and \
+            site[2].replace('.','',1).replace('-','',1).isdigit():
+                coordinates_data[site[0]] = [float(site[1]),float(site[2])]      
+    se_sites = {}
     se_list = []
     manager = Manager()
     se_result = manager.listSEs()['Value']
@@ -31,31 +32,28 @@ def get_se_list():
     if not se_health['OK']: 
         print "[WARNING]: No health info for SE sites"
     for se in sorted(se_result['Active']):
+        se_site = {}
         result = gConfig.getOptionsDict('/Resources/StorageElements/' + se)
         result2 = gConfig.getOptionsDict('/Resources/StorageElements/' + se \
                                         + '/AccessProtocol.1')
         if result['OK'] and result2['OK']:
             result = result['Value']
             result2 = result2['Value']
-            se_site = SE_site(se)
-            se_site.host = [result2['Host'],result2['Port'],result2['WSUrl']]
-            if 'SpaceToken' in result2:
-                se_site.token = result2['SpaceToken']
-            se_site.path = result2['Path']
-            se_site.read = result['ReadAccess']
-            se_site.write = result['WriteAccess']
+            se_site = result2
+            se_site['Read'] = result['ReadAccess']
+            se_site['Write'] = result['WriteAccess']
 
-            if se_site.name.split('-')[0] in coordinates_data:
-                se_site.coordinates = coordinates_data[se_site.name.split('-')[0]]
+            if se.split('-')[0] in coordinates_data:
+                se_site['Coordinates'] = coordinates_data[se.split('-')[0]]
             else:
-                print "[WARNING]: No location info for " + se_site.name
-            se_list.append(se_site)
+                print "[WARNING]: No location info for " + se
+            se_sites[se] = se_site
 
-            if se_health['OK'] and se_site.name in se_health['Value']:
-                se_site.health = se_health['Value'][se_site.name] 
+            if se_health['OK'] and se in se_health['Value']:
+                se_site['Health'] = se_health['Value'][se][0]
 
 
-    return se_list
+    return se_sites
 
 def get_health():
     #result = service.getAllStorageElementStatus()
@@ -63,6 +61,6 @@ def get_health():
     return result
 
 if __name__ == "__main__":
-    result = get_health()
+    result = get_se_sites()
     pprint(result)
     #get_se_list()
